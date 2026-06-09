7a44843b35973895f0bad14759b4a1117b5e5bfd
Rajeev Rawat <rajeevrawat2100@email.com>
2026-06-09 17:23:09 +0530

    fix: all endpoint

--- src/components/ListingPage.jsx
+++ src/components/ListingPage.jsx
@@ -7,7 +7,7 @@ const ListingPage = () => {
   const [meetupType, setMeetupType] = useState("");
 
   const { data, loading, error } = useFetch(
-    "https://meetup-backend-gc9i.vercel.app/meetup"
+    "https://meetup-backend-iota.vercel.app/meetup"
   );
 
   console.log(data)
